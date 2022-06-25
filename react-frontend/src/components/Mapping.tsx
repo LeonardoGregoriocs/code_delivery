acb6cba0251f3e942bbd48d767e5d9c28702c252
Leonardogregoriocs <leonardogregoriocs@gmail.com>
2022-06-25 17:53:13 -0300

    cria base do frontend para aplicacao

---
+++ react-frontend/src/components/Mapping.tsx
@@ -0,0 +1,25 @@
+import { Button, Grid, MenuItem, Select } from "@material-ui/core";
+
+type Props = {};
+export const Mapping = (props: Props) => {
+    return (
+        <Grid container>
+            <Grid item xs={12} sm={3}>
+                <form>
+                    <Select fullWidth>
+                        <MenuItem value= "">
+                            <em>Selecione uma corrida</em>
+                        </MenuItem>
+                        <MenuItem value= "1">
+                            <em>Primeiro</em>
+                        </MenuItem>
+                    </Select>
+                    <Button type="submit" color="primary" variant="contained">Iniciar uma corrida</Button>
+                </form>
+            </Grid>
+            <Grid item xs={12} sm={9}>
+                <div id="map"></div>
+            </Grid>
+        </Grid>
+    );
+};
